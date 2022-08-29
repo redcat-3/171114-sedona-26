6031377b00ec73a9440a6a9d2327433fc2d1b46e
redcat <redcat-3@yandex.ru>
2022-08-29 23:38:41 +0800

    вставила видео

---
+++ source/js/video.js
@@ -0,0 +1,25 @@
+let video = document.querySelectorAll('.video');
+
+function setupVideo(video) {
+  let link = video.querySelector('.video__link');
+  let button = video.querySelector('.video__button');
+  video.addEventListener('click', () => {
+    let iframe = createIframe();
+    link.remove();
+    button.remove();
+    video.appendChild(iframe);
+  });
+  link.removeAttribute('href');
+  video.classList.add('video--enabled');
+}
+
+function createIframe() {
+  let iframe = document.createElement('iframe');
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
+  iframe.setAttribute('src', 'https://www.youtube.com/watch?v=MB_iFDDNtrw');
+  iframe.classlist.add('video__media');
+  return iframe;
+}
+
+setupVideo(video);
