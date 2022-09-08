ac440e1f62754e368742e380ea06302016e68b32
redcat <redcat-3@yandex.ru>
2022-09-08 18:39:47 +0800

    Правки главной страницы

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,11 @@
 let headerNav = document.querySelector('.header-nav__wrapper');
 let navToggle = document.querySelector('.header-nav__toggle');
+let navHeader = document.querySelector('.header-nav');
+let hero = document.querySelector('.hero');
+
+navToggle.classList.remove('header-nav__toggle--disablejs');
+navHeader.classList.remove('header-nav--disablejs');
+hero.classList.remove('hero--disablejs');
 
 headerNav.classList.add('header-nav__wrapper--closed');
 if (headerNav.classList.contains('header-nav__wrapper--closed')) {
